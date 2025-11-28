e94a0f5d3d090e6290ade3542891d5f6b66fde66
ebeutz <ebeutz@ucsd.edu>
2025-11-28 10:34:57 +0000

    Remove conflicting package

--- rootfs/firstboot.sh
+++ rootfs/firstboot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # This file would be executed on first boot
+echo Setting up device...
 sudo btrfs device add /dev/disk/by-partlabel/userdata / -f
 sudo rm /etc/ssh/ssh_host_*
 sudo dpkg-reconfigure openssh-server
 sudo systemctl restart ssh
 sed -i '/firstboot.sh/d' ~/.profile
+echo Setup complete!
